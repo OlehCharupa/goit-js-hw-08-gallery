c64951bc722275fe64d292287bf50d964bc988a5
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-09-24 00:03:44 +0300

    hw08 добавил закрытие модального окна по клику и нажатии esc

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import gallery from "./gallery-items.js";
 
+const body = document.querySelector("body");
 const jsGallery = document.querySelector(".js-gallery");
 const divLightbox = document.querySelector(".js-lightbox");
+const divOverlay = document.querySelector(".lightbox__overlay");
 const imgModal = document.querySelector(".lightbox__image");
 const buttonClose = document.querySelector(".lightbox__button");
 
 gallery.map((el) => {
   jsGallery.innerHTML += `<li class="gallery__item"><a class="gallery__link" href="${el.preview}"><img class="gallery__image" src="${el.preview}" data-source="${el.original}" alt="${el.description}"/></a></li>`;
 });
+const galleryItem = document.querySelectorAll(".gallery__item");
 
 jsGallery.addEventListener("click", (e) => {
   e.preventDefault();
@@ -19,4 +22,16 @@ jsGallery.addEventListener("click", (e) => {
 buttonClose.addEventListener("click", () => {
   divLightbox.classList.remove("is-open");
   imgModal.src = "";
+});
+
+divOverlay.addEventListener("click", () => {
+  divLightbox.classList.remove("is-open");
+  imgModal.src = "";
+});
+
+body.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    divLightbox.classList.remove("is-open");
+    imgModal.src = "";
+  }
 });
